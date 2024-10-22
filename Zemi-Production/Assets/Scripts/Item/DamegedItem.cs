fa2381bfc572dd747dbeb9ac0c6e80e340fd3652
Hayato222 <g2227070@st.t-kougei.ac.jp>
2024-10-23 03:39:32 +0900

    Stageに関するアイテムを追加

---
+++ Zemi-Production/Assets/Scripts/Item/DamegedItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Item
+{
+    public class DamegedItem : MonoBehaviour
+    {
+        private Vector3 startPosition;
+
+        private GameObject repopItem;
+
+        private void Start()
+        {
+            startPosition = transform.position;
+            repopItem = this.gameObject;
+        }
+
+        private void Repop()
+        {
+            Instantiate(repopItem,startPosition,Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.CompareTag("Floor"))
+            {
+                Repop();
+            }
+        }
+    }
+}
