a89df1a1ca67d4e2e3487c9e9eb6c37f74379124
ebifry22 <g2227011@st.t-kougei.ac.jp>
2024-10-31 00:15:57 +0900

    add/AttributeChange

---
+++ Zemi-Production/Assets/Scripts/AttributeChange.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttributeChange : MonoBehaviour
+{
+    [SerializeField] GameObject throwObj;
+
+    [SerializeField] Material lockMaterial;
+    [SerializeField] Material scissorsMaterial;
+    [SerializeField] Material paperMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeLock()
+    {
+        throwObj.GetComponent<Renderer>().material = lockMaterial;
+    }
+
+    public void ChangeScissors()
+    {
+        throwObj.GetComponent<Renderer>().material = scissorsMaterial; ;
+    }
+
+    public void ChangePaper()
+    {
+        throwObj.GetComponent<Renderer>().material = paperMaterial; ;
+    }
+}
