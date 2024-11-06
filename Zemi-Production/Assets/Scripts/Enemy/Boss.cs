3a7468c034353d87475e0ad281375c5f4e1c5c41
Hayato222 <g2227070@st.t-kougei.ac.jp>
2024-11-06 23:52:09 +0900

    bossの攻撃を実装

--- Zemi-Production/Assets/Scripts/Enemy/Boss.cs
+++ Zemi-Production/Assets/Scripts/Enemy/Boss.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using Item;
+using Player;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 namespace Enemy
 {
@@ -13,17 +15,66 @@ namespace Enemy
 
         [SerializeField] private float DamagedItemPower = 10;
 
+        public PlayerState player;
+        private int rnd = 0;
+        [SerializeField] private int randomValue = 5;
+        [SerializeField] private float coolTime = 5;
+        private float time;
+        private bool chooseNumber = false;
+        [SerializeField] private float chargeTime = 3; 
+        
+
         private void Start()
         {
             bossMaxHealth = bossHealth;
             BossHealthBar.value = bossHealth / bossMaxHealth;
         }
 
+        private void Update()
+        {
+            time += Time.deltaTime;
+            if (time > coolTime)
+            {
+                AttackTrigger();
+            }
+        }
+
+        private void AttackTrigger()
+        {
+            if (!chooseNumber)
+            {
+                rnd = Random.Range(1, randomValue + 1);
+                chooseNumber = true;
+                Debug.Log("rnd = " + rnd);
+            }
+            
+            if (rnd == randomValue)
+            {
+                if (time > coolTime + chargeTime)
+                {
+                    Attack();
+                    Debug.Log("Attack");
+                    rnd = 0;
+                }
+            }
+            else
+            {
+                chooseNumber = false;
+                time = 0;
+            }
+        }
+
+
+        private void Attack()
+        {
+            player.Attacked();
+            player.UpdateHelthBar();
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             bossHealth -= other.gameObject.GetComponent<DamegedItem>().AttackDamage(); 
             BossHealthBar.value = bossHealth / bossMaxHealth;
-            
         }
     }
 }
---
+++ Zemi-Production/Assets/Scripts/Enemy/Interface/IState.cs
@@ -0,0 +1,14 @@
+﻿namespace Enemy.Interface
+{
+    public enum StateType
+    {
+        Idle,
+        Attack,
+        Dead,
+    }
+    
+    public interface IState
+    {
+        
+    }
+}
