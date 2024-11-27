6de0ef679239367473857173b9dcbf227d558d1a
Hayato222 <g2227070@st.t-kougei.ac.jp>
2024-11-28 01:45:42 +0900

    敵に向かってオブジェクトを撃つスクリプトを実装

--- Zemi-Production/Assets/Scripts/GrabCheck.cs
+++ Zemi-Production/Assets/Scripts/GrabCheck.cs
@@ -6,22 +6,45 @@ public class GrabCheck : MonoBehaviour
 {
     private GameObject gesture;
     AttributeChange attributeChange;
+    private GameObject Bullet;
+    private GameObject Boss;
+    private bool shot = false;
+
+    [SerializeField] private float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
         gesture = GameObject.Find("Gesture");
         attributeChange=gesture.GetComponent<AttributeChange>();
+        Boss = GameObject.Find("Boss");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (shot && Bullet)
+        {
+            Bullet.transform.position =
+                Vector3.MoveTowards(Bullet.transform.position, 
+                    Boss.transform.position, speed * Time.deltaTime);
+        }   
     }
+
+    public void Shoot()
+    {
+        shot = true;
+    }
+
+    public void Cancel()
+    {
+        shot = false;
+    }
+    
 
     private void OnTriggerEnter(Collider other)
     {
         attributeChange.SetThrow(other.gameObject);
+        Bullet = other.gameObject;
     }
 
     private void OnTriggerExit(Collider other)
